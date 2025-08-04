f2f500b07850f1759c6dc097e9b4608307c25365
laxmithapa2 <laxmithapa.adtu@gmail.com>
2025-08-04 22:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function viewWork() {
+  alert("Redirecting to portfolio showcase...");
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+function contactMe() {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+}
